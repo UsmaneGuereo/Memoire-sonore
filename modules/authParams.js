16a863c3d9ea112e91617a6b041585cadb34a4fc
Ousmane Ndione <ndioneousmane9@gmail.com>
2025-12-09 05:01:51 +0100

    ajout du guide d'utilisation du module

--- modules/authParams.js
+++
@@ -1,8 +0,0 @@
-export const pa = {
-    'navbar':d3.select('#navbarMain'),
-    mail:'ndioneousmane9@gmail.com',
-    apiOmk:'http://localhost/omk_THyp_25-26_clone/api/',
-    ident: 'NDfWQB6jwVQEyR5jKS5njoP2K3IHmQm8',
-    key:'6Wib53Kon9CcKzHeliWapZ9ad9rb9JK1',
-    keyGitHub:''
-}
